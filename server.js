2e8d8b89c6e762cdf9c3bf5b843ec1a7a2feeffd
NNcryptoTest <ninicunicolae4@gmail.com>
2026-09-20 11:43:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const path = require("path");
 const { Server } = require("socket.io");
 
-const app = express();:
+const app = express();
 const fs = require("fs");
 app.get("/debug", (req, res) => {
   res.json({
